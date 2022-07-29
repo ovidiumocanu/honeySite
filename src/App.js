93bc0bc51ae6dae3e2307d88ac7c6b694e347116
obidu <ovidiu8mocanu@yahoo.com>
2022-07-29 21:05:11 +0300

    finished landing page, added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
-import LandingPage from './components/landingpage/Landing-Page';
+import "./App.css";
+import LandingPage from "./components/landingpage/Landing-Page";
+import NavBar from "./components/navbar/NavBar";
+import Footer from "./components/footer/footer";
+import AboutUs from "./components/aboutpage/AboutUs";
+import Contact from "./components/contactpage/Contact";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <LandingPage/>
+      <Router>
+        <NavBar />
+
+        <Routes>
+          <Route index element={<LandingPage />} />
+          {/* <Route path='/products' element={<Products/>}/> */}
+          <Route path="/aboutUs" element={<AboutUs />} />
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
